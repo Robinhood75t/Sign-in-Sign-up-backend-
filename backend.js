e384ab957e51df355123c13dfb887ce614a2ce13
Robinhood75t <161246802+Robinhood75t@users.noreply.github.com>
2025-08-22 09:26:58 +0530

    initial commit

---
+++ backend.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const axios = require("axios");
+
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+
+app.post("/sign-up",function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    users.push({
+        email,
+        password
+    });
+    res.json({
+        message: "signed up successfully"
+    })
+})
+
+app.post("/login",function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let user = users.find( user => user.email === email && user.password === password);
+    if(user){
+        res.json({
+            message: "login successfull"
+        });
+    }else{
+        res.json({
+            message: "wrong credentials"
+        })
+        alert("dosen't have any account matching to your credentials");
+    }
+})
+
+
+app.listen(3000, function(){
+    console.log("surver running");
+})
